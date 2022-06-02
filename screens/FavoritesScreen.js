968a073a948c990744f7df8ecd1049a8e3e84791
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-02 16:15:13 +0800

    feat: redux state management

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,16 @@
 import {View, Text, StyleSheet} from 'react-native';
-import {useContext} from 'react';
+import {useSelector} from 'react-redux';
+// import { useContext } from 'react';
 
 import MealsList from '../components/MealsList/MealsList';
-import {FavoritesContext} from '../store/context/favorites-context';
+// import { FavoritesContext } from '../store/context/favorites-context';
 import {MEALS} from '../data/dummy-data';
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
-  const favoriteMeals = MEALS.filter((meal) => favoriteMealsCtx.ids.includes(meal.id));
+  const favoriteMeals = MEALS.filter((meal) => favoriteMealIds.includes(meal.id));
 
   if (favoriteMeals.length === 0) {
     return (
