d2dc19d4d02acf2dd70163a9111751c3c686592d
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-02 14:27:01 +0800

    feat: meal item FlatList

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,31 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
 
+import MealItem from '../components/MealItem';
 import {MEALS} from '../data/dummy-data';
 
 function MealsOverviewScreen({route}) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
     </View>
   );
 }
