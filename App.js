1330ba8fdbded1071a9d67240fdb52d7febced15
Guanghui Wang <guanghui-wang@foxmail.com>
2022-06-02 14:31:36 +0800

    style: styling screen

--- App.js
+++ App.js
@@ -13,8 +13,20 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {backgroundColor: '#351401'},
+            headerTintColor: 'white',
+            contentStyle: {backgroundColor: '#3f2f25'},
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: 'All Categories',
+            }}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
